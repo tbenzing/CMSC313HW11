dd1664f9b057877df6715d06df4db017247c4b74
tbenzing <tbenzing4567@gmail.com>
2025-05-15 12:40:28 -0400

    Completed Draft of Main Code

--- translatetoascii.asm
+++ translatetoascii.asm
@@ -1,4 +1,3 @@
-
 section .data
 inputBuf db 0x83, 0x6A, 0x88, 0xDE, 0x9A, 0xC3, 0x54, 0x9A
 inputLen equ $ - inputBuf
@@ -8,3 +7,57 @@ newline db 0x0A
 
 section .bss
 outputBuf resb 80
+
+section .text
+global _start
+
+_start:
+    mov esi, inputBuf     ; Source pointer to inputBuf
+    mov edi, outputBuf    ; Destination pointer to outputBuf
+    mov ecx, inputLen     ; Number of bytes to convert
+
+convert_loop:
+    cmp ecx, 0
+    je convert_done       ; If done, exit loop
+
+    ; Load byte from input
+    mov al, byte [esi]
+
+    ; Upper nibble
+    mov ah, al
+    shr ah, 4
+    and ah, 0x0F
+    mov bl, byte [hexDigits + ah]
+    mov [edi], bl
+    inc edi
+
+    ; Lower nibble
+    and al, 0x0F
+    mov bl, byte [hexDigits + al]
+    mov [edi], bl
+    inc edi
+
+    ; Add space
+    mov byte [edi], ' '
+    inc edi
+
+    inc esi
+    dec ecx
+    jmp convert_loop
+
+convert_done:
+    dec edi
+    mov byte [edi], 0x0A   ; newline
+
+    ; write syscall: sys_write (eax=4)
+    mov eax, 4
+    mov ebx, 1          ; stdout
+    mov ecx, outputBuf
+    mov edx, edi
+    sub edx, outputBuf  ; length = edi - outputBuf
+    int 0x80
+
+    ; exit syscall: sys_exit (eax=1)
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
